ff4ea84214aaac50592018413b18811f3fd35bff
isilvac <isilvac@users.noreply.github.com>
2018-03-08 16:20:40 -0300

    inclusión de modulos
    librerias añadidas

---
+++ bot/index.js
@@ -0,0 +1,191 @@
+var builder = require('botbuilder');
+var siteUrl = require('./site-url');
+
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+
+// Welcome Dialog
+var MainOptions = {
+    Catalog: 'main_options_catalog',
+    Stores: 'main_options_stores',
+    SocialNetworks: 'main_options_sns'
+};
+
+var bot = new builder.UniversalBot(connector,
+    (session) => {
+        session.send('salute');
+        session.sendTyping();
+        setTimeout(function () {
+            session.beginDialog('menu');
+        }, 1000);
+    }
+);
+
+bot.dialog('greetings', [
+    function (session, args, next) {
+        session.dialogData.profile = args || {};
+        if (!session.dialogData.profile.name) {
+            session.beginDialog('askName');
+        } else {
+            next();
+        }
+    },
+    function (session, results, next) {
+        if (results.response) {
+            var name = session.dialogData.profile.name = results.response;
+            session.send('salute_with_name', name);
+            session.endDialogWithResult({ response: session.dialogData.profile })
+        }
+    }
+]);
+
+bot.dialog('askName', [
+    function (session) {
+        session.send(session.gettext('salute'));
+        setTimeout(function () {
+        builder.Prompts.text(session, session.gettext('name_request'));
+        }, 1000);
+    },
+    function (session, results) {
+        session.endDialogWithResult(results);
+    }
+]);
+
+bot.dialog('menu', [
+    function (session) {
+        if (localizedRegex(session, [MainOptions.Catalog]).test(session.message.text)) {
+            // Order Flowers
+            return session.beginDialog('catalog:/');
+        }
+
+        if (localizedRegex(session, [MainOptions.Stores]).test(session.message.text)) {
+            // Order Flowers
+            return session.beginDialog('stores:/');
+        }
+
+        if (localizedRegex(session, [MainOptions.SocialNetworks]).test(session.message.text)) {
+            // Order Flowers
+            return session.beginDialog('social-networks:/');
+        }
+
+        var welcomeCard = new builder.HeroCard(session)
+            .title('welcome_title')
+            .subtitle('welcome_subtitle')
+            .images([
+                new builder.CardImage(session)
+                    .url('https://dfp2hfrf3mn0u.cloudfront.net/233/233512_original_1.jpg')
+                    .alt('bike_store')
+            ])
+            .buttons([
+                builder.CardAction.imBack(session, session.gettext(MainOptions.Catalog), MainOptions.Catalog),
+                builder.CardAction.imBack(session, session.gettext(MainOptions.Stores), MainOptions.Stores),
+                builder.CardAction.imBack(session, session.gettext(MainOptions.SocialNetworks), MainOptions.SocialNetworks)
+            ]);
+        session.send(new builder.Message(session)
+            .addAttachment(welcomeCard));
+    }
+]).triggerAction({
+    matches: /^menu$/i
+});
+
+bot.dialog('bye', [
+    (session) => { session.send(session.gettext('bye')).endConversation(); }
+]).triggerAction({
+    matches: /^adios$|^chao$|^bye$/i
+});
+
+// Enable Conversation Data persistence
+bot.set('persistConversationData', true);
+
+// Set default locale
+bot.set('localizerSettings', {
+    botLocalePath: './bot/locale',
+    defaultLocale: 'es'
+});
+
+// Sub-Dialogs
+bot.library(require('./dialogs/catalog').createLibrary());
+bot.library(require('./dialogs/product-selection').createLibrary());
+bot.library(require('./dialogs/stores').createLibrary());
+bot.library(require('./dialogs/social-networks').createLibrary());
+bot.library(require('./dialogs/settings').createLibrary());
+bot.library(require('./dialogs/help').createLibrary());
+
+// Validators
+bot.library(require('./validators').createLibrary());
+
+// Trigger secondary dialogs when 'settings' or 'support' is called
+bot.use({
+    botbuilder: function (session, next) {
+        var text = session.message.text;
+
+        var settingsRegex = localizedRegex(session, ['main_options_settings']);
+        var supportRegex = localizedRegex(session, ['main_options_talk_to_support', 'help']);
+
+        if (settingsRegex.test(text)) {
+            // interrupt and trigger 'settings' dialog 
+            return session.beginDialog('settings:/');
+        } else if (supportRegex.test(text)) {
+            // interrupt and trigger 'help' dialog
+            return session.beginDialog('help:/');
+        }
+
+        // continue normal flow
+        next();
+    }
+});
+
+// Send welcome when conversation with bot is started, by initiating the root dialog
+bot.on('conversationUpdate', function (message) {
+    if (message.membersAdded) {
+        message.membersAdded.forEach(function (identity) {
+            if (identity.id === message.address.bot.id) {
+                bot.beginDialog(message.address, '/');
+            }
+        });
+    }
+});
+
+// Cache of localized regex to match selection from main options
+var LocalizedRegexCache = {};
+function localizedRegex(session, localeKeys) {
+    var locale = session.preferredLocale();
+    var cacheKey = locale + ":" + localeKeys.join('|');
+    if (LocalizedRegexCache.hasOwnProperty(cacheKey)) {
+        return LocalizedRegexCache[cacheKey];
+    }
+
+    var localizedStrings = localeKeys.map(function (key) { return session.localizer.gettext(locale, key); });
+    var regex = new RegExp('^(' + localizedStrings.join('|') + ')', 'i');
+    LocalizedRegexCache[cacheKey] = regex;
+    return regex;
+}
+
+// Connector listener wrapper to capture site url
+var connectorListener = connector.listen();
+function listen() {
+    return function (req, res) {
+        // Capture the url for the hosted application
+        // We'll later need this url to create the checkout link 
+        var url = req.protocol + '://' + req.get('host');
+        siteUrl.save(url);
+        connectorListener(req, res);
+    };
+}
+
+// Other wrapper functions
+function beginDialog(address, dialogId, dialogArgs) {
+    bot.beginDialog(address, dialogId, dialogArgs);
+}
+
+function sendMessage(message) {
+    bot.send(message);
+}
+
+module.exports = {
+    listen: listen,
+    beginDialog: beginDialog,
+    sendMessage: sendMessage
+};
---
+++ services/products.js
@@ -0,0 +1,175 @@
+var _ = require('lodash');
+var builder = require('botbuilder');
+var Promise = require('bluebird');
+
+var allCategories =
+    [{
+        name: 'MONTANA',
+        subtitle: 'Escapa a la montana!',
+        imageUrl: 'http://www.scottchile.com/imagenes/bannermontana.jpg'
+    },
+    {
+        name: 'CARRETERA',
+        subtitle: 'Vuela sobre el asfalto...',
+        imageUrl: 'http://www.scottchile.com/imagenes/bannercarretera.jpg'
+    },
+    {
+        name: 'URBANAS',
+        subtitle: 'Muevete en la ciudad',
+        imageUrl: 'http://www.scottchile.com/imagenes/bannercityurban.jpg'
+    },
+    {
+        name: 'MUJER',
+        subtitle: 'Para ellas',
+        imageUrl: 'http://www.scottchile.com/imagenes/bannermujer.jpg'
+    },
+    {
+        name: 'JUNIOR',
+        subtitle: 'Para los mas pequenos de la casa',
+        imageUrl: 'http://www.scottchile.com/imagenes/bannerjunior.jpg'
+    }];
+
+var mountainBikes =
+    [{
+        name: 'SCALE RC 900 SL',
+        subtitle: 'Cross Country',
+        imageUrl: 'https://dfp2hfrf3mn0u.cloudfront.net/265/265201_210191_png_overview_4.png',
+        buttonUrl: 'https://www.scott-sports.com/es/es/product/sco-bike-scale-rc-900-sl-m-null?article=265201007'
+    },
+    {
+        name: 'SCALE RC 900 WORLD CUP',
+        subtitle: 'Cross Country',
+        imageUrl: 'https://dfp2hfrf3mn0u.cloudfront.net/265/265202_210184_png_overview_8.png',
+        buttonUrl: 'https://www.scott-sports.com/es/es/product/sco-bike-scale-rc-900-world-cup-xl-null?article=265202009'
+    },
+    {
+        name: 'SCALE 915',
+        subtitle: 'Cross Country',
+        imageUrl: 'https://dfp2hfrf3mn0u.cloudfront.net/265/265205_209107_png_overview_4.png',
+        buttonUrl: 'https://www.scott-sports.com/es/es/product/sco-bike-scale-915-s-null?article=265205006'
+    },
+    {
+        name: 'SCALE 920',
+        subtitle: 'Cross Country',
+        imageUrl: 'https://dfp2hfrf3mn0u.cloudfront.net/265/265210_217313_png_overview_8.png',
+        buttonUrl: 'https://www.scott-sports.com/es/es/product/sco-bike-scale-920-eu-s-null?article=265210006'
+    }];
+
+var highwayBikes =
+    [{
+        name: 'PLASMA PREMIUM',
+        subtitle: 'Aero',
+        imageUrl: 'https://dfp2hfrf3mn0u.cloudfront.net/265/265326_210247_png_overview_4.png',
+        buttonUrl: 'https://www.scott-sports.com/es/es/product/sco-bike-plasma-premium-l57-null?article=265326027'
+    },
+    {
+        name: 'PLASMA RC',
+        subtitle: 'Aero',
+        imageUrl: 'https://dfp2hfrf3mn0u.cloudfront.net/265/265327_210242_png_overview_4.png',
+        buttonUrl: 'https://www.scott-sports.com/es/es/product/sco-bike-plasma-rc-m54-null?article=265327022'
+    },
+    {
+        name: 'FOIL PREMIUM DISC',
+        subtitle: 'Aero',
+        imageUrl: 'https://dfp2hfrf3mn0u.cloudfront.net/265/265330_210246_png_overview_4.png',
+        buttonUrl: 'https://www.scott-sports.com/es/es/product/sco-bike-foil-premium-disc-l56-null?article=265330023'
+    },
+    {
+        name: 'FOIL RC',
+        subtitle: 'Aero',
+        imageUrl: 'https://dfp2hfrf3mn0u.cloudfront.net/265/265331_210245_png_overview_4.png',
+        buttonUrl: 'https://www.scott-sports.com/es/es/product/sco-bike-foil-rc-s52-null?article=265331021'
+    }];
+
+var urbanBikes =
+    [{
+        name: 'SILENCE EVO',
+        subtitle: 'City & Urban',
+        imageUrl: 'https://dfp2hfrf3mn0u.cloudfront.net/265/265451_208819_png_overview_4.png',
+        buttonUrl: 'https://www.scott-sports.com/es/es/product/sco-bike-silence-evo-l-null?article=265451008'
+    },
+    {
+        name: 'SILENCE 20 LADY',
+        subtitle: 'City & Urban',
+        imageUrl: 'https://dfp2hfrf3mn0u.cloudfront.net/265/265455_208823_png_overview_4.png',
+        buttonUrl: 'https://www.scott-sports.com/es/es/product/sco-bike-silence-20-lady-l-null?article=265455008'
+    },
+    {
+        name: 'SILENCE 30 MEN',
+        subtitle: 'City & Urban',
+        imageUrl: 'https://dfp2hfrf3mn0u.cloudfront.net/265/265456_208825_png_overview_4.png',
+        buttonUrl: 'https://www.scott-sports.com/es/es/product/sco-bike-silence-30-men-l-null?article=265456008'
+    }];
+
+var defaultBikes =[{
+    name: 'Sin bicicletas por el momento',
+    subtitle: 'Somos un bot al que no le alcanza la plata para tanta bicicleta...',
+    imageUrl: null
+}];
+
+var productsService = {
+    // Categories
+    getCategories: function (pageNumber, pageSize) {
+        return pageItems(pageNumber, pageSize, allCategories);
+    },
+
+    // Get Single Category
+    getCategory: function (categoryName) {
+        var category = _.find(allCategories, ['name', categoryName]);
+        return Promise.resolve(category);
+    },
+
+    // Products
+    getProducts: function (categoryName, pageNumber, pageSize) {
+        var products;
+        switch (categoryName) {
+            case 'MONTANA':
+                products = mountainBikes;
+                break;
+            case 'CARRETERA':
+                products = highwayBikes;
+                break;
+            case 'URBANAS':
+                products = urbanBikes;
+                break;
+            default:
+                products = defaultBikes;
+                break;
+        }
+        return pageItems(pageNumber, pageSize, products);
+    },
+
+    // Get Single Product
+    getProduct: function (productName, session) {
+        var category;
+        switch (session.dialogData.category.name) {
+            case 'MONTANA':
+                category = mountainBikes;
+                break;
+            case 'CARRETERA':
+                category = highwayBikes;
+                break;
+            case 'URBANAS':
+                category = urbanBikes;
+                break;
+            default:
+                category = defaultBikes;
+                break;
+        }
+        var product = _.find(category, ['name', productName]);
+        return Promise.resolve(product);
+    }
+};
+
+// helpers
+function pageItems(pageNumber, pageSize, items) {
+    var pageItems = _.take(_.drop(items, pageSize * (pageNumber - 1)), pageSize);
+    var totalCount = items.length;
+    return Promise.resolve({
+        items: pageItems,
+        totalCount: totalCount
+    });
+}
+
+// export
+module.exports = productsService;
